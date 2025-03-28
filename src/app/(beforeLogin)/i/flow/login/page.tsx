75e88a4972ed75d36a3a78f3ca3d0c670d8a630c
seunggi <hsg77689469.gamil.com>
2025-03-28 23:32:31 +0900

    src/app/page.tsx

---
+++ src/app/(beforeLogin)/i/flow/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>로그인 모달</div>
+  )
+}
+
+export default page
---
+++ src/app/(beforeLogin)/i/flow/signup/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>회원가입모달</div>
+  )
+}
+
+export default page
